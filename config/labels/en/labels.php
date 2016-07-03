5dda298dc082551c981de8bed34320185c97cc24
maickon <maickonmaickon@hotmail.com>
2016-07-03 19:31:16 -0300

    add config/labels versao en, jp e es

---
+++ config/labels/en/labels.php
@@ -0,0 +1,187 @@
+<? Php
+// Site name
+const SITE_NAME = 'Help RPG';
+// symbols
+const HORIZONTAL_BAR = '|';
+// Home page - meta tags
+const META_DESCRIPTION = 'Help rpg - Make registration sheets RPG, items, weapons, skills, generate dungeon maps, name of cities, characters, kingdoms and more.';
+const META_AUTHOR = 'Maickon Rangel';
+const META_KEYWORDS = 'Sheets RPG, utilities RPG, Utilities, Tools RPG, weapons Registration RPG, Spells Register RPG, Artifact Register for RPG, D & D, Rifts RPG, World of RPG darkness, Mutants & Masterminds RPG, Traveller RPG Call of Cthulhu RPG, Hackmaster RPG, GURPS RPG, Savage Worlds RPG Pathfinder RPG ';
+// Titles for slide
+const SLIDE_TITLE_01 = 'All you need for your RPG is here!';
+const SLIDE_TITLE_02 = 'Share your RPG here! Become a Helper ';
+const SLIDE_TITLE_03 = 'Use our utilities and be happy with the RPG Help!';
+// Slide messages
+const SLIDE_MESSAGE_01 = 'The Help RPG is a new website that brings together everything you need to assist you in your RPG, we have chips, utilities, spells, weapons, armor, etc. All you need we offer here. ';
+const SLIDE_MESSAGE_02 = 'Create an account and start now to register chips, weapons, items, powerful artifacts and more. Share the best of your adventures with us and will be part of this community. ';
+const SLIDE_MESSAGE_03 = 'We have many utilities that will help you at the right time, we have several generators to increase the productivity of your session. What are you waiting for? then run back and use our utilities. ';
+// Free text
+const FREE_TEXT_01 = 'Hover the mouse on one of our utilities below for more information.';
+// Utilities - data
+const UTILITIES_DICE = 'data Rolador - utilities';
+const META_DESCRIPTION_DICE = 'Rolador data - Help RPG.';
+const META_KEYWORDS_DICE = 'roller data, d4, d6, d8, d10, d12, d100, roll data, data.';
+const DICE_OPTION_CONFIC2X4 = '2x4 (2 lines and 4 columns)';
+const DICE_OPTION_CONFIC2X6 = '2x6 (2 rows and 6 columns)';
+const DICE_OPTION_CONFIC3X3 = '3x3 (3 rows and 3 columns)';
+const DICE_OPTION_CONFIC7X1 = '7x1 (7 rows and 1 column)';
+const DICE_CLEAR = 'Clear';
+const DICE_ROLL_DICE = 'Roll All';
+const DICE_CONFIG = 'Configuration';
+const DICE_ROLL_PLUS = 'Roll +';
+const DICE_GO = 'Go';
+const DICE_SHOW_TOTAL = 'Full View';
+// Utilities - names
+const UTILITIES_NAME = 'Generator names - utilities';
+const META_DESCRIPTION_NAME = 'Name Generator - Help RPG.';
+const META_KEYWORDS_NAME = 'names, place names, class names, breed names, cultural names, other names, ship names, superhero names';
+// Menu items
+const HOME = 'Home';
+const CONTACT = 'CONTACT';
+const REGISTER = 'REGISTER';
+const ABOUT = 'ON';
+const HOW_TO_USE = 'USING';
+const FOREIGN_VERSION = 'VERSION OF FOREIGN';
+const DONATION = 'DONATION';
+const PARTNERSHIP = 'PARTNERSHIP';
+const WHO_AM_I = 'THAT I AM';
+const FACEBOOK = 'PAGE ON FACEBOOK';
+const YOUTUBE = 'CHANNEL ON YOUTUBE';
+const BLOG = 'BLOG HELP RPG';
+// Utilities menu name
+const NAME_PLACES = 'Places';
+const NAME_CLASS = 'classes';
+const NAME_RACES = 'Races';
+const NAME_CULTURAL = 'Cultural';
+const NAME_OTHERS = 'Other';
+const NAME_OPTIONS_ANIMALS = 'Pets';
+const NAME_OPTIONS_ANGELS = 'Angels';
+const NAME_OPTIONS_DEMONS = 'Demons';
+const NAME_OPTIONS_DWARVES = 'Dwarves';
+const NAME_OPTIONS_ELVES = 'Elves';
+const NAME_OPTIONS_HALFLING = 'Halfling';
+const NAME_OPTIONS_HOBBITS = 'Hobbits';
+const NAME_OPTIONS_ORCS = 'Orcs';
+const NAME_OPTIONS_MALE_RATS = 'Mouse Men';
+const NAME_OPTIONS_HUMAN_FEMALE = 'Human - Women';
+const NAME_OPTIONS_HUMAN_MALE = 'Human - Men';
+const NAME_OPTIONS_ARABIC = 'Arabic';
+const NAME_OPTIONS_ASIAN = 'Asia';
+const = NAME_OPTIONS_AZTEC Asteca;
+const NAME_OPTIONS_EGYPTIAN = 'Egyptians';
+const NAME_OPTIONS_SPANISH = 'Spanish';
+const NAME_OPTIONS_FRENCH = 'French';
+const NAME_OPTIONS_BELGIUM_DUTCH = 'Bélgico - Netherlands';
+const NAME_OPTIONS_SCOTIA_PICTISH = 'Scotland (Pictish)';
+const NAME_OPTIONS_FRANCOS_FEMALE = 'Francs (Female)';
+const NAME_OPTIONS_FRANCOS_MALE = 'Francs (Male)';
+const NAME_OPTIONS_RUSSIAN_FEMALE = 'Russian (Female)';
+const NAME_OPTIONS_RUSSIAN_MALE = 'Russian (Male)';
+const NAME_OPTIONS_ROMAN_FEMALE = 'Romano (Female)';
+const NAME_OPTIONS_ROMAN_MALE = 'Roman (Male)';
+const NAME_OPTIONS_GAELIC_CELTIC_FEMALE = 'Gaelic Celtic (Female)';
+const NAME_OPTIONS_GAELIC_CELTIC_MALE = 'Gaelic Celtic (Male)';
+const NAME_OPTIONS_GAELIC_SCOTTISH_FEMALE = 'Scottish Gaelic (Female)';
+const NAME_OPTIONS_GAELIC_SCOTTISH_MALE = 'Scottish Gaelic (Male)';
+const NAME_OPTIONS_ITALY_REMANESCISTA_FEMALE = 'Italia Remanescista (Female)';
+const NAME_OPTIONS_ITALY_REMANESCISTA_MALE = 'Italia Remanescista (Male)';
+const NAME_OPTIONS_GERMANIC_FEMALE = 'Germanicus (Female)';
+const NAME_OPTIONS_GERMANIC_MALE = 'Germanicus (Male)';
+const NAME_OPTIONS_PERSIAN_FEMALE = 'Persian (Female)';
+const NAME_OPTIONS_PERSIAN_MALE = 'Persian (Male)';
+const NAME_OPTIONS_ANGLO_SAXON_LATINIZED_FEMALE = 'Anglo Saxon - Latinized (Female)';
+const NAME_OPTIONS_ANGLO_SAXON_LATINIZED_MALE = 'Anglo Saxon - Latinized (Male)';
+const NAME_OPTIONS_ANGLO_SAXON_ENGLISH_FEMALE = 'Anglo Saxon - English; 15 centuries; 16:17 (Female) ';
+const NAME_OPTIONS_ANGLO_SAXON_ENGLISH_MALE = 'Anglo Saxon - English; 15 centuries; 16:17 (Male) ';
+const NAME_OPTIONS_AMERICAN_USA_AB = 'American USA (from A to B)';
+const NAME_OPTIONS_AMERICAN_USA_CD = 'American USA (from C to D)';
+const NAME_OPTIONS_AMERICAN_USA_EH = 'American USA (E to H)';
+const NAME_OPTIONS_AMERICAN_USA_IL = 'American USA (R to L)';
+const NAME_OPTIONS_AMERICAN_USA_MO = 'American USA (M to O)';
+const NAME_OPTIONS_AMERICAN_USA_PR = 'American USA (R to R)';
+const NAME_OPTIONS_AMERICAN_USA_ST = 'American USA (S to T)';
+const NAME_OPTIONS_AMERICAN_USA_UZ = 'American USA (U to Z)';
+const NAME_OPTIONS_VILLAGE = 'Pueblos';
+const NAME_OPTIONS_CITIES = 'Cities';
+const NAME_OPTIONS_KINGDOMS = 'Kingdoms';
+const NAME_OPTIONS_PLANETS = 'Planets';
+const NAME_OPTIONS_CONSTELLATIONS = 'Constellations';
+const NAME_OPTIONS_GALAXIES = 'Galaxies';
+const NAME_OPTIONS_GOTHIC = 'Gothic';
+const NAME_OPTIONS_PIRATES = 'Pirates';
+const NAME_OPTIONS_KINGS = 'Kings';
+const NAME_OPTIONS_SHIPS = 'Spacecraft';
+const NAME_OPTIONS_HEROES = 'Super Heroes';
+const NAME_OPTIONS_HEROES_BR = 'Super Heroes Br';
+const NAME_OPTIONS_SURNAME_EN = 'Nicknames in English';
+const NAME_OPTIONS_DIVINE_TITLES = 'Titles Divine';
+const NAME_OPTIONS_GODS = 'gods';
+const NAME_OPTIONS_LAST_NAME = 'Name';
+const NAME_OPTIONS_FAMILY = 'family name';
+const NAME_OPTIONS_ORC_HALF = 'Half Orc';
+const NAME_OPTIONS_SURNAME = 'Aplelidos';
+const NAME_OPTIONS_CLAN = 'clan name';
+const NAME_OPTIONS_TIELFLING = "Tiefling";
+//Home page
+const RECORDS = 'RECORDS';
+const CHARACTER_SHEETS = 'CHIPS';
+const USERS_REGISTERED = 'USERS';
+const FILE_REGISTERED = 'CHIPS REGISTERED';
+const MONSTERS_REGISTERED = 'MONSTERS REGISTERED';
+const FILE_MONSTERS = 'MONSTERS FORMS';
+const ITEMS_REGISTERED = 'REGISTERED ITEMS';
+const FILE_PLAYER_CHARACTER = 'PLAYER CHARACTER';
+const FILE_NPC_CHARACTER = 'NPC CHARACTER';
+const WEAPONS = 'GUNS';
+const ARMOR = 'ARMOUR';
+const ARTIFACTS = 'ARTIFACTS';
+const SPELLS = 'SPELLS';
+const TALENTS = 'TALENT';
+const SKILLS = 'SKILLS';
+const RANDOM_FILE = 'SHEET RANDOM';
+const RANDOM_MONSTERS = 'RANDOM MONSTER';
+const STORIES = 'STORIES';
+const ADVENTURES = 'ADVENTURES';
+const TALES = 'TALES';
+const CHRONICLES = 'CHRONIC';
+const SCENARIO = 'SCENARIOS';
+const ROLL_DICE = 'DATA roller';
+const GENERATOR_WORLDS = 'WORLDS GENERATOR';
+const Bestiary = 'Bestiary';
+const world_map = 'MUNDI MAP';
+const GENERATOR_BASE_FILE = 'BASE SHEETS GENERATOR';
+const GENERATOR_FEATURES = 'GENERATOR FEATURES';
+const GENERATOR_NAMES = 'NAME GENERATOR';
+const GENERATOR_ADVENTURE = 'ADVENTURES GENERATOR';
+const UTILITIES = 'UTILITIES';
+const CHARACTER = 'CHARACTERS';
+// Utilitaries page
+const DISABLE_MODE_DRAW = 'Disable mode draw';
+// Web social
+const FACEBOOK_PAGE = 'Facebook';
+const TWITTER_PAGE = 'Twitter';
+const GOOGLE_PAGE = 'Google +';
+const YOU_TUBE_PAGE = 'YOUTUBE';
+const SOCIAL_TITLE = 'We are social';
+const SOCIAL_MSG = 'Follow the news of the Help RPG in social media.';
+// Contact
+const CONTACT_TITLE = 'Quick Contact';
+const CONTACT_EMAIL_LABEL = 'Email';
+const CONTACT_CALL_LABEL = 'Tel';
+const CONTACT_SKYPE_LABEL = 'Skype';
+const CONTACT_EMAIL_CONTENT = 'helprpg.br@gmail.com';
+const CONTACT_CALL_CONTENT = '55 22 99614-9758 ';
+const CONTACT_SKYPE_CONTENT = 'maickon.rangel';
+// Address
+const ADDRESS = 'Address';
+const ADDRESS_CONTENT = 'RJ, São João da Barra';
+const ADDRESS_COUNTRY_CONTENT = 'Brazil';
+const COPYRIGHT = 'Help RPG & copy 2014-2016 |';
+const TEMPLATE_NAME = 'by DesignBootstrp';
+// Others utititaries
+const TABLETOPAUDIO = 'Tabletop Audio';
+const Pyromancers = 'Pyromancers';
+const INKARNATE = 'Inkarnate';
+// errors
+const ERROR_FILE_NOT_FOUND = 'class file can not be found.';
+const ERROR_NAME_NOT_FOUND = 'name can not be found.';
