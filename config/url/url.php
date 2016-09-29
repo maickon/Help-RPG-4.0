2e5f56cad56fc37eab886055efc0b983a3285ba5
maickon <maickonmaickon@hotmail.com>
2016-09-28 23:54:32 -0300

    Commit de teste

--- config/url/url.php
+++ config/url/url.php
@@ -8,19 +8,19 @@ require 'social/social.php';
 require 'txt/aventuras/aventuras.php';
 require 'txt/tavernas/tavernas.php';
 
-const LOG_PATH = URL_BASE_INTERNAL . 'config/log/log.txt';
+define('LOG_PATH', LOG_PATH = URL_BASE_INTERNAL . 'config/log/log.txt');
 // img url
-const IMG_ICON_PATH = URL_BASE . 'app/assets/img/icons/';
-const CONFIG_DB_PATH = 'config/db/';
-const CONFIG_ERRORS_PATH = 'config/errors/';
+define('IMG_ICON_PATH', URL_BASE . 'app/assets/img/icons/');
+define('CONFIG_DB_PATH', 'config/db/');
+define('CONFIG_ERRORS_PATH', 'config/errors/');
 
 // view URL fichas
-const SHEETS_NPC_URL = URL_BASE . 'fichas/npc';
-const SHEETS_MONSTERS_URL = URL_BASE . 'fichas/monstros';
+define('SHEETS_NPC_URL', URL_BASE . 'fichas/npc');
+define('SHEETS_MONSTERS_URL', URL_BASE . 'fichas/monstros');
 
 // web servece
-const DICE_SERVICE_URL = URL_BASE . 'dados-service/';
-const SHEETS_NPC_SERVICE_URL = URL_BASE . 'service/fichas/npc/ded';
-const SHEETS_MONSTERS_SERVICE_URL = URL_BASE . 'service/fichas/monstros/ded';
+define('DICE_SERVICE_URL', URL_BASE . 'dados-service/');
+define('SHEETS_NPC_SERVICE_URL', URL_BASE . 'service/fichas/npc/ded');
+define('SHEETS_MONSTERS_SERVICE_URL', URL_BASE . 'service/fichas/monstros/ded');
 // txt config files
-const TXT_NAMES = URL_BASE_INTERNAL . 'config/txt/nomes';
+define('TXT_NAMES', URL_BASE_INTERNAL . 'config/txt/nomes');
