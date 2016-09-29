7b19b464cbc409aabe66a6ddd8821aa2bae55996
maickon <maickonmaickon@hotmail.com>
2016-09-29 00:20:23 -0300

    Commit de teste

--- config/initialize.php
+++ config/initialize.php
@@ -1,20 +1,25 @@
 <?php
 require "url/url.php";
 
-$url_base = URL_BASE_INTERNAL;
+define('URL_BASE_INTERNAL_DB', URL_BASE_INTERNAL . 'config/db/db.php');
+define('URL_BASE_INTERNAL_LABELS', URL_BASE_INTERNAL . 'config/labels/pt-br/labels.php');
+define('URL_BASE_INTERNAL_CONTROLLER', URL_BASE_INTERNAL . '/app/controllers/');
+define('URL_BASE_INTERNAL_MODEL', URL_BASE_INTERNAL . '/app/models/');
+define('URL_BASE_INTERNAL_LIB', URL_BASE_INTERNAL . '/lib/');
+define('URL_BASE_INTERNAL_HELPER', URL_BASE_INTERNAL . '/app/helpers/');
 
-require $url_base . "config/db/db.php";
-require $url_base . "config/labels/pt-br/labels.php";
+require URL_BASE_INTERNAL_DB;
+require URL_BASE_INTERNAL_LABELS;
 
 
 function __autoload($class_name){
 	$divide_name = explode('_', $class_name);
 	$module_name = strtolower($divide_name[0]);
 	
-	$controllers_path =	$url_base . "/app/controllers/";
-	$model_path = $url_base . "/app/models/";
-	$lib_path =	$url_base . "/lib/";
-	$helper_path =	$url_base . "/app/helpers/";
+	$controllers_path =	URL_BASE_INTERNAL_CONTROLLER;
+	$model_path = URL_BASE_INTERNAL_MODEL;
+	$lib_path =	URL_BASE_INTERNAL_LIB;
+	$helper_path =	URL_BASE_INTERNAL_HELPER;
 
 	switch ($divide_name[1]) {
 		case 'Controller':
